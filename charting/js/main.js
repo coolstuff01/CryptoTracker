44e47e02e1911b6316e03e729ac416e1034139ba
Nikolay <nsitnikov1@gmail.com>
2018-02-19 21:12:06 -0500

    Clean main.js

--- charting/js/main.js
+++ charting/js/main.js
@@ -11,7 +11,6 @@ var logic_currency = "btc"; // base currency for underlying logic
 var response = []; // variable to store response from coinmarketcap
 var valid_tks = []; // valid tokens
 var currs = {}; // list for all currencies in portfolio
-var jjj;
 
 var amounts_btc = {}; // amounts of all currencies in bitcoin
 var amounts_loc = {}; // amounts of all currencies in local currency
